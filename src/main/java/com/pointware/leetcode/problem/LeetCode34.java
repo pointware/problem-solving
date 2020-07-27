75bde0567c9ca42124f82338206ddc489c32b3f7
pointware <pwangkk@gmail.com>
2020-07-28 01:51:22 +0900

    [leetcode] Find First and Last Position of Element in Sorted Array

---
+++ src/main/java/com/pointware/leetcode/problem/LeetCode34.java
@@ -0,0 +1,8 @@
+package com.pointware.leetcode.problem;
+
+/**
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+ */
+public interface LeetCode34 {
+    int[] searchRange(int[] nums, int target);
+}
